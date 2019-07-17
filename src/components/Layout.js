0aa04651773639524eb9846a09f10d3409351346
Brian Emilius <brian@brianemilius.com>
2019-07-17 21:54:18 +0200

    add footer to layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import SiteHeader from "./SiteHeader";
+import SiteFooter from "./SiteFooter";
 
 export default function Layout({ children }) {
   return (
     <div itemScope={true} itemType="//schema.org/WebPage">
       <SiteHeader />
       {children}
+      <SiteFooter />
     </div>
   );
 }
