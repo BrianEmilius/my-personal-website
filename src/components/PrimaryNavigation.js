a3a3b96255fc33140e148a0e55767fc9418f2b9f
Brian Emilius <brian@brianemilius.com>
2019-07-18 15:30:37 +0200

    add RSS feed link

--- src/components/PrimaryNavigation.js
+++ src/components/PrimaryNavigation.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "gatsby";
+import { Link, StaticQuery, graphql } from "gatsby";
 import Styled from "styled-components";
 
 const Ul = Styled.ul`
@@ -7,20 +7,49 @@ const Ul = Styled.ul`
 	padding: 0;
   margin: 0;
   line-height: 5vh;
+  display: flex;
 `;
 
 const Anchor = Styled(Link)`
-	color: hsl(0, 0%, 96%);
+  color: hsl(0, 0%, 96%);
+  text-decoration: none;
+  &:not(last-of-type) {
+    padding: 0 1em;
+  }
 `;
 
-export default function PrimaryNavigation() {
+const Image = Styled.img`
+  height: 1rem;
+  width: auto;
+  display: inline-block;
+`;
+
+const Component = function({ rss }) {
   return (
     <nav>
       <Ul>
         <li>
           <Anchor to="/blog">Blog</Anchor>
         </li>
+        <li>
+          <Anchor to="/rss.xml">
+            <Image src={rss.publicURL} alt="RSS feed icon" title="RSS Feed" />{" "}
+            RSS Feed
+          </Anchor>
+        </li>
       </Ul>
     </nav>
   );
+};
+
+const query = graphql`
+  query {
+    rss: file(base: { eq: "subscribe-rss-button.svg" }) {
+      publicURL
+    }
+  }
+`;
+
+export default function PrimaryNavigation() {
+  return <StaticQuery query={query} render={data => <Component {...data} />} />;
 }
