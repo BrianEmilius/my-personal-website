b42407f4b383c25f083c053037d787de00f0b231
Brian Emilius <brian@brianemilius.com>
2019-07-18 15:58:23 +0200

    remove RSS feed link from menu

--- src/components/PrimaryNavigation.js
+++ src/components/PrimaryNavigation.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, StaticQuery, graphql } from "gatsby";
+import { Link } from "gatsby";
 import Styled from "styled-components";
 
 const Ul = Styled.ul`
@@ -18,38 +18,14 @@ const Anchor = Styled(Link)`
   }
 `;
 
-const Image = Styled.img`
-  height: 1rem;
-  width: auto;
-  display: inline-block;
-`;
-
-const Component = function({ rss }) {
+export default function PrimaryNavigation() {
   return (
     <nav>
       <Ul>
         <li>
           <Anchor to="/blog">Blog</Anchor>
         </li>
-        <li>
-          <Anchor to="/rss.xml">
-            <Image src={rss.publicURL} alt="RSS feed icon" title="RSS Feed" />{" "}
-            RSS Feed
-          </Anchor>
-        </li>
       </Ul>
     </nav>
   );
-};
-
-const query = graphql`
-  query {
-    rss: file(base: { eq: "subscribe-rss-button.svg" }) {
-      publicURL
-    }
-  }
-`;
-
-export default function PrimaryNavigation() {
-  return <StaticQuery query={query} render={data => <Component {...data} />} />;
 }
