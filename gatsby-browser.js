06f6f6bd013d7c79e2dbccb588022a6509d90f1e
Brian Emilius <brian@brianemilius.com>
2019-07-20 12:53:17 +0200

    remove ntlify oauth support

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,4 +3,3 @@ import "./src/fonts/fonts.css";
 import "./src/images/external-link-symbol.svg";
 import "prismjs/themes/prism-tomorrow.css";
 import "gitment/style/default.css";
-import "netlify-auth-providers/lib/netlify.js";
