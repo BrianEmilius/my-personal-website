5d0d63dee67d63362d4083e8df1f6057c20ed397
Brian Emilius <brian@brianemilius.com>
2019-07-27 10:27:16 +0200

    add subscribe component

---
+++ src/components/SubscribeButton.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { graphql, StaticQuery } from "gatsby";
+import Styled from "styled-components";
+
+const Subscribe = Styled.a`
+  border: 1px solid grey;
+  border-radius: 5px;
+  padding: 1em;
+  background: linear-gradient(hsl(120, 50%, 50%), hsl(120, 50%, 30%));
+  display: inline-block;
+  margin-bottom: 1em;
+  text-decoration: none;
+  color: hsl(0, 0%, 97%);
+`;
+
+const query = graphql`
+  query {
+    rss: file(relativePath: { eq: "subscribe-rss-button.svg" }) {
+      publicURL
+    }
+  }
+`;
+
+const Component = function({ rss }) {
+  return (
+    <Subscribe href="/feed.rss">
+      <img
+        src={rss.publicURL}
+        alt="RSS feed icon"
+        style={{ height: "1em", width: "auto" }}
+      />{" "}
+      Subscribe to this blog
+    </Subscribe>
+  );
+};
+
+export default function SubscribeButton() {
+  return <StaticQuery query={query} render={data => <Component {...data} />} />;
+}
