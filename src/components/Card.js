bdb157f4e72922b88f5ffbd5b46cd18b30ea7d23
Brian Emilius <brian@brianemilius.com>
2019-07-28 21:18:09 +0200

    change card link style

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,34 +1,24 @@
 import React from "react";
 import { Link } from "gatsby";
-import Styled from "styled-components";
 
 import CoverImage from "./CoverImage";
 import Tags from "./Tags";
-
-const Article = Styled.article`
-  margin-bottom: 3em;
-  padding-bottom: 1em;
-  border-bottom: 3px solid hsl(240, 30%, 20%);
-`;
-
-const Published = Styled.p`
-  font-size: .8em;
-  color: grey;
-  text-align: center;
-`;
+import styles from "./Card.module.css";
 
 export default function Card({ excerpt, frontmatter, fields }) {
   return (
-    <Article>
-      <Published>
+    <article className={styles.article}>
+      <p className={styles.article__published}>
         <time dateTime={frontmatter.date}>{frontmatter.date}</time>
-      </Published>
+      </p>
       <CoverImage frontmatter={frontmatter} />
       <h1>
-        <Link to={fields.slug}>{frontmatter.title}</Link>
+        <Link to={fields.slug} className={styles.article__heading}>
+          {frontmatter.title}
+        </Link>
       </h1>
       {excerpt}
       <Tags frontmatter={frontmatter} />
-    </Article>
+    </article>
   );
 }
