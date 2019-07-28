b269fba8c7fd927d27a4e4f1bb1bcf7996356e8b
Brian Emilius <brian@brianemilius.com>
2019-07-28 20:50:02 +0200

    add tags module

---
+++ src/components/Tags.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "gatsby";
+
+import styles from "./Tags.module.css";
+
+export default function Tags({ frontmatter }) {
+  if (frontmatter.tags) {
+    return (
+      <div className={styles.tagContainer}>
+        {frontmatter.tags.map(function(tag, index) {
+          return (
+            <Link
+              to={`/tags/${tag}`}
+              key={index}
+              className={styles.tagContainer__tag}
+            >
+              {tag}
+            </Link>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
