3ce9bc137e403c8ac47b1dff7833f1a0f46a978e
Brian Emilius <brian@brianemilius.com>
2019-08-04 11:38:01 +0200

    add latest posts to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Layout from "../components/Layout";
 import Hero from "../components/Hero";
+import LatestPosts from "../components/LatestPosts";
 
-export default function Index() {
-  return (
-    <Layout title="Home">
-      <Hero />
-    </Layout>
-  );
+export default class Index extends Component {
+  render() {
+    return (
+      <Layout title="Home">
+        <div style={{ "grid-area": "main" }}>
+          <Hero />
+          <LatestPosts />
+        </div>
+      </Layout>
+    );
+  }
 }
