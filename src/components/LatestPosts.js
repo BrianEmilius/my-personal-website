c89a0fc8a2e30ffd4c6bb22213462657e8714138
Brian Emilius <brian@brianemilius.com>
2019-08-04 11:37:48 +0200

    add latest posts component

---
+++ src/components/LatestPosts.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { graphql, StaticQuery } from "gatsby";
+
+// eslint-disable-next-line
+import { fluidImage } from "./queryFragments";
+import Card from "./Card";
+import styles from "./LatestPosts.module.css";
+
+const query = graphql`
+  query postsQuery {
+    allMarkdownRemark(
+      limit: 6
+      sort: { fields: frontmatter___date, order: DESC }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            title
+            date
+            tags
+            image {
+              ...fluidImage
+            }
+          }
+          excerpt
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default function LatestPosts() {
+  return (
+    <StaticQuery
+      query={query}
+      render={data => (
+        <section className={styles.latestPosts}>
+          <h1 className={styles.latestPosts__heading}>Latest Blogposts</h1>
+          {data.allMarkdownRemark.edges.map(post => (
+            <Card {...post.node} />
+          ))}
+        </section>
+      )}
+    />
+  );
+}
