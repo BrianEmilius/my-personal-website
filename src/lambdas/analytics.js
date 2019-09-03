6d2fe2b8973084c1c77b132a56188681a228cbef
Brian Emilius <brian@brianemilius.com>
2019-09-03 09:17:37 +0200

    add temporary analytics lambda function

--- src/lambdas/analytics.js
+++ src/lambdas/analytics.js
@@ -1,6 +1,7 @@
 const faunadb = require("faunadb");
 
 exports.handler = function(event, context, callback) {
+  console.log(JSON.parse(event.body));
   callback(null, {
     statusCode: 200,
     body: "analytics, yo"
